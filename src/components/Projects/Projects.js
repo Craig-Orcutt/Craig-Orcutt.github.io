0ce801b739021efa4cd610ff2cb47e012a28007a
Craig Orcutt <craigaorcutt@gmail.com>
2018-05-31 11:18:32 -0500

    broke up projects into seperate components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,44 +1,21 @@
 import React from "react";
-import './projects.css';
+import QuickAssess from "./QuickAssess";
+import ScoreCaddy from "./ScoreCaddy";
+import CheckIn from "./CheckIn";
+import "./projects.css";
 
 class Projects extends React.Component {
   render() {
     return (
-      <div id="projects">
+      <div>
         <h2>Projects</h2>
-        <p>
-          Skate ipsum dolor sit amet, betty boned out coping smith grind. Fakie
-          out disaster ollie sponsored. Primo slide Upland airwalk wheels launch
-          ramp. Slap maxwell hard flip 1080 Stacy Peralta full pipe. Transfer
-          coper handplant 270. Saran Wrap smith grind invert Jim Phillips Kevin
-          Harris. Tail hang up heel flip hang-up. Cab flip 360 boned out coper.
-          Nose bump Tim May hand rail gap kickturn. Pump risers 50-50 chicken
-          wing Chris Haslam.{" "}
-        </p>
-
-        <p>
-          Casper street half-cab Skull and Sword sponsored quarter pipe hang-up.
-          900 Tod Swank full-cab nollie dude masonite pump. Carve kidney
-          fastplant transition Kevin Harris lip. Goofy footed 180 Kien Lieu hang
-          up boneless late rad. 720 mongo Bonite varial concave full pipe wall
-          ride. Birdie hardware Tracker handplant Slap air downhill. Speed
-          wobbles axle set noseblunt slide transfer face plant World Industries
-          mini ramp. Vert Bones Brigade rails air slide aerial fast plant. Goofy
-          footed slam half-cab deck death box hanger. Nose bump nose blunt carve
-          blunt Shawn Dutton ollie north tuna-flip. Slap maxwell manual kidney
-          bearings flypaper Justin Regan full-cab.{" "}
-        </p>
-
-        <p>
-          Transworld concave transition shinner camel back. Wallows late
-          boardslide feeble rip grip. Airwalk slam cab flip fast plant China
-          Banks. Noseblunt slide Transworld 900 melancholy transfer. Fastplant
-          slob air rocket air late Vernon Courtland Johnson. Nose bump gap Kevin
-          Harris slide. Hang-up stalefish yeah face plant. Layback skate or die
-          face plant rocket air. Ray Barbee shoveit flypaper rocket air helipop.
-          Alley oop 720 carve egg plant. Rock and roll bearings rip grip wax.
-          Late casper slide Zorlac boneless mini ramp.{" "}
-        </p>
+        <div id="projects">
+          <div>
+            <QuickAssess />
+            <ScoreCaddy />
+            <CheckIn />
+          </div>
+        </div>
       </div>
     );
   }
